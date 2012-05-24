123e82e640927651a4609aa5162d1c16237fc222
Mrunal Patel <mpatel@redhat.com>
2012-05-23 18:34:22 -0700

    Use db port while connecting.

--- php/common.php
+++ php/common.php
@@ -38,8 +38,9 @@
      $host   = $_ENV["OPENSHIFT_NOSQL_DB_HOST"];
      $user   = $_ENV["OPENSHIFT_NOSQL_DB_USERNAME"];
      $passwd = $_ENV["OPENSHIFT_NOSQL_DB_PASSWORD"];
+     $port = $_ENV["OPENSHIFT_NOSQL_DB_PORT"];
 
-     $uri = "mongodb://" . $user . ":" . $passwd . "@" . $host;
+     $uri = "mongodb://" . $user . ":" . $passwd . "@" . $host . ":" . $port;
      $mongo = new Mongo($uri);
      return $mongo;
   }

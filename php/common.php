af8385a4fd2e4f078b0cd7b55e086bddf8ffec57
Ram Ranganathan <ramr@redhat.com>
2012-10-04 18:28:04 -0700

    Fix variables for dev/typeless, mongodb version and add hooks+cron files.

--- php/common.php
+++ php/common.php
@@ -35,10 +35,10 @@
 
 
   function get_db_connection() {
-     $host   = $_ENV["OPENSHIFT_NOSQL_DB_HOST"];
-     $user   = $_ENV["OPENSHIFT_NOSQL_DB_USERNAME"];
-     $passwd = $_ENV["OPENSHIFT_NOSQL_DB_PASSWORD"];
-     $port   = $_ENV["OPENSHIFT_NOSQL_DB_PORT"];
+     $host   = $_ENV["OPENSHIFT_MONGODB_DB_HOST"];
+     $user   = $_ENV["OPENSHIFT_MONGODB_DB_USERNAME"];
+     $passwd = $_ENV["OPENSHIFT_MONGODB_DB_PASSWORD"];
+     $port   = $_ENV["OPENSHIFT_MONGODB_DB_PORT"];
 
      $uri = "mongodb://" . $user . ":" . $passwd . "@" . $host . ":" . $port;
      $mongo = new Mongo($uri);

516a899402bacd4cf75e4a9432ad54fce731727c
yusmawita <yusma.wita@gmail.com>
2015-08-08 16:23:16 +0700

    coba pake table

--- php/index.php
+++ php/index.php
@@ -13,6 +13,39 @@ if($i % 2 == 0) {
 }
 }
 ?>
+<table border="1">
+<tr>
+<td width="200px">Bilangan Genap</td>
+<td width="200px">Bilangan Ganjil</td>
+</tr>
+<tr>
+<td style="text-align:center">
+<%
+for(int i=1;i<=100;i++){
+        if(i%2==0){
+        echo(i+"<br/>");}
+}
+%>
+</td>
+<td style="text-align:center">
+<%
+for(int i=1;i<=100;i++){
+        if(i%2==1){
+        echo(i+"<br/>");}
+}
+}
+%>
+</td>
+<td style="text-align:center">
+<%
+for(int i=1;i<=100;i++){
+        if(i%2==1){
+        echo(i+"<br/>");}
+}
+%>
+</td>
+</tr>
+</table>
 
 
   </body>

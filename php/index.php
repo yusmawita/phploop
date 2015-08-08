dc74e0932d3c66a6710fc6f04d36a30802849ae8
yusmawita <yusma.wita@gmail.com>
2015-08-08 16:32:35 +0700

    tambah index

--- php/index.php
+++ php/index.php
@@ -18,41 +18,6 @@ if($i % 2 == 0) {
 
 
 
-<table border="1">
-<tr>
-<td width="200px">Bilangan Genap</td>
-<td width="200px">Bilangan Ganjil</td>
-</tr>
-<tr>
-<td style="text-align:center">
-<?php
-for($i=1;$i<=100;$i++){
-        if($i%2==0){
-        echo($i.' <br> Bilangan Genap');
-}
-}
-?>
-</td>
-<td style="text-align:center">
-<?php
-for($i=1;$i<=100;$i++){
-        if($i%2==1){
-        echo(i+' Bilangan Ganjil <br/>');}
-}
-}
-?>
-</td>
-<td style="text-align:center">
-<%
-for($i=1;$i<=100;$i++){
-        if($i%2==1){
-        echo(i+'<br/>');}
-}
-%>
-</td>
-</tr>
-</table>
-
 
   </body>
 </html>
